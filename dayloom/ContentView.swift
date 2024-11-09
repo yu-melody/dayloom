c2c4c137fa023c4344e05e473992a1cfef6c9f77
Melody Yu <yu.melo@northeastern.edu>
2024-11-09 10:41:07 -0500

    Initial Commit

---
+++ dayloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dayloom
+//
+//  Created by Melody Yu on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dayloom/dayloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  dayloomApp.swift
+//  dayloom
+//
+//  Created by Melody Yu on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct dayloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
